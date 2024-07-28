db579c825a0b3476b37a281d122f2d3cfb72da4b
Akeem22 <90483300+Akeem22@users.noreply.github.com>
2024-07-28 18:15:24 +0100

    changes to the hero page sentence

--- src/Pages/SlideShow/Slideshow.js
+++ src/Pages/SlideShow/Slideshow.js
@@ -29,7 +29,7 @@ const Slideshow = () => {
           <div className="row">
             <div className="col-md-10">
               <div className="heading">
-                Serving over 25,000 businesses, multiple industry awards, rated A+ by the BBB.
+                Revolutionize your communication with our reliable and affordable VoIP solutions.
               </div>
               <a href="pricing.html" className="btn1zz btn-hvr-dar2k" tabindex="-1">Read more</a>
               <a href="pricing.html" className="btn1zz btn-hvr-dar2k" data-target="page-bottom" tabindex="-1">Request Consultation</a>
